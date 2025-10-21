8ae472fb5f770fcb673a715b27bb5da7cbfab3b9
mdmahfuz307 <njnirob2019@gmail.com>
2025-10-21 21:47:01 +0600

    Tailwind Color Add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-    <div className="text-5xl text-red-500 text-center">Hello Nirob</div>
+    <div className="text-5xl text-red-500 text-center font-bold">Hello Nirob</div>
     </>
   )
 }
